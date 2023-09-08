c4925cd781c591de432a58e29fa20fd78a02bf38
Skyline7760 <132228814+Skyline7760@users.noreply.github.com>
2023-09-08 13:22:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,64 +10,57 @@ const firebaseConfig = {
   // Your Firebase configuration here
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Reference to your Firebase Realtime Database
 const database = firebase.database();
 
-// Create an empty array to store data for the chart
-const chartData = {
-  labels: [], // Timestamps
-  values: []  // Values
-};
-
-// Get a reference to the canvas element and create the Chart.js chart
+// Initialize a Chart.js chart
 const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: chartData.labels,
-    datasets: [{
-      label: 'Value',
-      data: chartData.values,
-      borderColor: 'rgba(75, 192, 192, 1)',
-      fill: false
-    }]
-  },
-  options: {
-    responsive: true,
-    scales: {
-      x: {
-        type: 'linear',
-        position: 'bottom'
-      },
-      y: {
-        min: 0,
-        max: 100
-      }
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],  // Timestamps will go here
+        datasets: [{
+            label: 'Real-time Data',
+            data: [],  // Data values will go here
+            borderColor: 'blue',
+            borderWidth: 2,
+            fill: false
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom'
+            },
+            y: {
+                beginAtZero: true
+            }
+        }
     }
-  }
 });
 
-// Listen for changes in your Firebase Realtime Database
-database.ref('your_data_path').on('value', (snapshot) => {
-  const data = snapshot.val();
-  
-  // Extract timestamp and value from the Firebase data
-  const timestamp = data.timestamp;
-  const value = data.value;
+// Function to update the chart with new data
+function updateChart(timestamp, value) {
+    chart.data.labels.push(timestamp);
+    chart.data.datasets[0].data.push(value);
+    
+    // Remove old data if the chart becomes too long
+    if (chart.data.labels.length > 20) {
+        chart.data.labels.shift();
+        chart.data.datasets[0].data.shift();
+    }
+    
+    chart.update();
+}
 
-  // Update the chart data
-  chartData.labels.push(timestamp);
-  chartData.values.push(value);
-
-  // Remove old data points if you want to limit the number of points displayed
-  if (chartData.labels.length > 10) {
-    chartData.labels.shift();
-    chartData.values.shift();
-  }
-
-  // Update the chart
-  myChart.update();
+// Listen for changes in your Firebase database
+database.ref('/test/envelopes').on('child_added', (snapshot) => {
+    const data = snapshot.val();
+    const timestamp = data.timestamp;
+    const value = data.value;
+    
+    // Call the updateChart function to add new data points
+    updateChart(timestamp, value);
 });
